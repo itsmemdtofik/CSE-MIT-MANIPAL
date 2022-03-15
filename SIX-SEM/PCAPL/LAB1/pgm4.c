cf91df743623a99f1561037a25071965f17a93cd
taufique siddiqui <63937933+tofiksiddiqui@users.noreply.github.com>
2022-03-15 22:08:13 +0530

    Update pgm4.c

--- SIX-SEM/PCAPL/LAB1/pgm4.c
+++ SIX-SEM/PCAPL/LAB1/pgm4.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     int size;
     char str[100] = "HeLLO";
     char ch;
-
+    size = strlen(str);
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -18,11 +18,13 @@ int main(int argc, char *argv[])
         if(i == myRank){
 
             ch = str[i];
-            printf("The rank is = [ %d ] and character is = [ %c ]", i, ch);
-            if(ch >= 'A' && ch >= 'Z'){
+            
+            if(ch >= 'A' && ch <= 'Z'){
                 ch = ch + 32;
-            }else if(ch <= 'A' && ch <= 'Z'){
+                printf("The rank is = [ %d ] and character is = [ %c ]", i, ch);
+            }else if(ch >= 'a' && ch <= 'z'){
                 ch = ch - 32;
+                printf("The rank is = [ %d ] and character is = [ %c ]", i, ch);
             }
             str[i] = ch;
         }
